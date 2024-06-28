3cf9600aa2ad47e51c8328450a9968924d415d83
tn9th8 <tn9th8@gmail.com>
2024-06-28 09:52:55 +0700

    init project

---
+++ src/test/java/vn/nhannt/jobhunter/JobhunterApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.nhannt.jobhunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobhunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
