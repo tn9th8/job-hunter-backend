9caec465ad2824ef23662c35d1c0b1139f5d2375
tn9th8 <tn9th8@gmail.com>
2024-08-12 15:57:01 +0700

    create: role, permission entity

---
+++ src/main/java/vn/nhannt/jobhunter/domain/entity/Permission.java
@@ -0,0 +1,80 @@
+package vn.nhannt.jobhunter.domain.entity;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import vn.nhannt.jobhunter.util.SecurityUtil;
+
+@Entity
+@Table(name = "permissions")
+@SQLDelete(sql = "UPDATE permissions SET deleted = true WHERE id=?")
+@SQLRestriction("deleted = false")
+@Getter
+@Setter
+public class Permission implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "name field is not null")
+    private String name;
+
+    @NotBlank(message = "apiPath field is not null")
+    private String apiPath;
+
+    @NotBlank(message = "method field is not null")
+    private String method;
+
+    @NotBlank(message = "module field is not null")
+    private String module;
+
+    // FK
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    // log
+    @CreationTimestamp
+    private String createdBy;
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+    private String updatedBy;
+
+    private boolean deleted = Boolean.FALSE;
+
+    @PrePersist
+    private void setUserBeforeCreation() {
+        this.createdBy = this.updatedBy = SecurityUtil.getCurrentUserLogin().isPresent()
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : null;
+    }
+
+    @PreUpdate
+    private void setUserBeforeUpdate() {
+        this.updatedBy = SecurityUtil.getCurrentUserLogin().isPresent()
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : null;
+    }
+}
---
+++ src/main/java/vn/nhannt/jobhunter/domain/entity/Role.java
@@ -0,0 +1,81 @@
+package vn.nhannt.jobhunter.domain.entity;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import vn.nhannt.jobhunter.util.SecurityUtil;
+
+@Entity
+@Table(name = "roles")
+@SQLDelete(sql = "UPDATE roles SET deleted = true WHERE id=?")
+@SQLRestriction("deleted = false")
+@Getter
+@Setter
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "name field is not null")
+    private String name;
+
+    private String description;
+    private boolean active;
+
+    // FK
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Permission> permissions;
+
+    // log
+    @CreationTimestamp
+    private Instant createdAt;
+
+    private String createdBy;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+
+    private String updatedBy;
+
+    private boolean deleted = Boolean.FALSE;
+
+    // hooks
+    @PrePersist
+    private void setUserBeforeCreation() {
+        this.createdBy = this.updatedBy = SecurityUtil.getCurrentUserLogin().isPresent()
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : null;
+    }
+
+    @PreUpdate
+    private void setUserBeforeUpdate() {
+        this.updatedBy = SecurityUtil.getCurrentUserLogin().isPresent()
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : null;
+    }
+
+}
