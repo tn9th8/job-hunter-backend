4d929a840cfa039978de5f27add8bfec6ef0ff2b
tn9th8 <tn9th8@gmail.com>
2024-08-23 10:27:36 +0700

    update: interceptor to check permission

---
+++ src/main/java/vn/nhannt/jobhunter/util/error/PermissionException.java
@@ -0,0 +1,8 @@
+package vn.nhannt.jobhunter.util.error;
+
+public class PermissionException extends Exception {
+    // Constructor that accepts a message
+    public PermissionException(String message) {
+        super(message);
+    }
+}
