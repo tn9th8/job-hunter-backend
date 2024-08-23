64ac7cd79d8128955ff55e685bd4652702bc32d1
tn9th8 <tn9th8@gmail.com>
2024-08-23 07:41:35 +0700

    update: security config

--- src/main/java/vn/nhannt/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/nhannt/jobhunter/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import javax.crypto.spec.SecretKeySpec;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -46,14 +47,16 @@ public class SecurityConfiguration {
 
         final String[] whiteArray = {
                 "/", "/storage/**",
-                "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/auth/register",
-                "/api/v1/companies/**", "api/v1/jobs/**" };
+                "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/auth/register" };
 
         http
                 .csrf(c -> c.disable())
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers(whiteArray).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/companies").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/jobs").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/resumes").permitAll()
                         .anyRequest().authenticated())
                 // config jwt => client must send access token
                 .oauth2ResourceServer((oauth2) -> oauth2
