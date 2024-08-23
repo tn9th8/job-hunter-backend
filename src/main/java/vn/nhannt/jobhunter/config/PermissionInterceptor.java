1d67859942f0b2bbff4ee166f801826e6b207ece
tn9th8 <tn9th8@gmail.com>
2024-08-23 09:36:44 +0700

    config: intercepter to check permission

---
+++ src/main/java/vn/nhannt/jobhunter/config/PermissionInterceptor.java
@@ -0,0 +1,63 @@
+package vn.nhannt.jobhunter.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.HandlerMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import vn.nhannt.jobhunter.domain.entity.Permission;
+import vn.nhannt.jobhunter.domain.entity.Role;
+import vn.nhannt.jobhunter.domain.entity.User;
+import vn.nhannt.jobhunter.service.UserService;
+import vn.nhannt.jobhunter.util.SecurityUtil;
+import vn.nhannt.jobhunter.util.error.UniqueException;
+
+public class PermissionInterceptor implements HandlerInterceptor {
+    @Autowired
+    UserService userService;
+
+    @Override
+    @Transactional
+    public boolean preHandle(
+            HttpServletRequest request,
+            HttpServletResponse response, Object handler)
+            throws Exception {
+        String path = (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+        String requestURI = request.getRequestURI();
+        String httpMethod = request.getMethod();
+        System.out.println(">>> RUN preHandle");
+        System.out.println(">>> path= " + path);
+        System.out.println(">>> httpMethod= " + httpMethod);
+        System.out.println(">>> requestURI= " + requestURI);
+
+        String email = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+
+        if (email != null && !email.isEmpty()) {
+            User user = this.userService.findByUsername(email);
+            if (user != null) {
+                Role role = user.getRole();
+                if (role != null) {
+                    List<Permission> permissions = role.getPermissions();
+                    boolean isAllow = permissions.stream()
+                            .anyMatch(item -> item.getApiPath().equals(path)
+                                    && item.getMethod().equals(httpMethod));
+
+                    if (isAllow == false) {
+                        throw new UniqueException("Bạn không có quyền truy cập endpoint này.");
+                    }
+                } else {
+                    throw new UniqueException("Bạn không có quyền truy cập endpoint này.");
+                }
+            }
+        }
+
+        return true;
+
+    }
+}
